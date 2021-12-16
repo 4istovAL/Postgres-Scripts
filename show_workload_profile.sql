957d9e0f518a0c64df6865ad4c49013fca6c0446
4istovAL <96228569+4istovAL@users.noreply.github.com>
2021-12-16 13:52:28 +0300

    Update show_workload_profile.sql

--- show_workload_profile.sql
+++ show_workload_profile.sql
@@ -1,3 +1,4 @@
+-- Show active sessions
 select usename
 , state
 ,backend_type
@@ -10,3 +11,24 @@ where state='active'
 and backend_type<>'parallel worker'
 group by usename, state, query,backend_type
 order by count(1) desc;
+
+select 
+usename
+, state
+,backend_type
+, min(query_start)
+, max(query_start)
+,query ,
+count(1)
+from pg_stat_activity
+where state like '%idle%'
+and backend_type<>'parallel worker'
+group by usename, state, query,backend_type
+order by min(query_start) desc;
+
+-- Count active users
+select usename,state, count(*)
+from pg_stat_activity
+where state='active'
+group by usename, state
+order by 3 desc,1;
